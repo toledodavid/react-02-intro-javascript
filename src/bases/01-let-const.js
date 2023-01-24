7c088f21dbbe0aa34363e1d72747f3d11098c52b
David Toledo <dtoledoz0207@gmail.com>
2023-01-24 12:57:22 -0600

    Let and Const

---
+++ src/bases/01-let-const.js
@@ -0,0 +1,16 @@
+// let and const
+
+const firstName = 'David';
+const lastName = 'Toledo'
+
+
+let someValue = 5;
+someValue = 4;
+
+console.log(firstName, lastName);
+
+
+if (true) {
+  let firstName = 'Pedro';
+  console.log(firstName);
+}
