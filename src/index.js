7982ebe54372948a6a1b38314a29a86264199d7d
David Toledo <dtoledoz0207@gmail.com>
2023-01-31 10:36:21 -0600

    Add heroes array and use import and export to the data. Also use find and filter array methods

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('Hello World');
+import { heroes } from './data/heroes';
+
+
+const getHeroById = (id) => {
+  return heroes.find(hero => hero.id === id);
+}
+
+console.log(getHeroById(3));
+
+const getHeroesByOwner = (owner) => {
+  return heroes.filter(heroe => heroe.owner === owner);
+}
+
+console.log(getHeroesByOwner('Marvel'));
