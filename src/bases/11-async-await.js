f2a1209792207e71743ae08ce574109031dab549
David Toledo <dtoledoz0207@gmail.com>
2023-01-31 13:12:42 -0600

    async - await

---
+++ src/bases/11-async-await.js
@@ -0,0 +1,18 @@
+
+
+const getImage = async () => {
+  const apiKey = 'FMH46HzHk8wFS4OcWPBUXvpm7eYt14bF';
+
+  try {
+    const response = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+    const { data } = await response.json();
+    const { url } = data.images.original;
+    const img = document.createElement('img');
+    img.src = url;
+    document.body.append(img);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getImage();
