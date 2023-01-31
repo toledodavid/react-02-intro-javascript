aee9ab6310313ef0a5294667e479d966688697f4
David Toledo <dtoledoz0207@gmail.com>
2023-01-31 10:58:31 -0600

    Add multiple exports and default export

---
+++ src/bases/08-import-export.js
@@ -0,0 +1,17 @@
+import heroes, { owners } from '../data/heroes';
+
+
+const getHeroById = (id) => {
+  return heroes.find(hero => hero.id === id);
+}
+
+console.log(getHeroById(3));
+
+const getHeroesByOwner = (owner) => {
+  return heroes.filter(heroe => heroe.owner === owner);
+}
+
+console.log(getHeroesByOwner('Marvel'));
+
+
+console.log(owners);
